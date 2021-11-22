5be971b0bb6b855af2a14abea5c3095757bc7216
JosRodriguezNC <josrodriguez@student.neumont.edu>
2021-11-22 09:00:25 -0700

    yeet

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -30,5 +30,12 @@ exports.createUser = async (req, res) => {
     const insertResult = await collection.insertOne(person);
     client.close();
     console.log(req.body.name + ' added');
-    res.redirect('/'); // change path to the login page's path
+    res.redirect('/index'); // change path to the login page's path
 }
+
+//index reference
+exports.index = (req, res) => {
+    res.render('index', {
+        title: 'Index page'
+    });
+};
